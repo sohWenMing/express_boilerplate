9893f585b7f67d456c35cebd4a7652f55db4053a
Soh Wen Ming <wenming.soh@temus.com>
2023-10-20 10:25:29 +0800

    json message

--- myApp.js
+++ myApp.js
@@ -9,6 +9,9 @@ console.log("Hello World");
 
 const staticPath = __dirname + '/public';
 app.use('/public', express.static(staticPath));
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json");
+})
 const absolutePath = __dirname + '/views/index.html';
 app.get('/', function(req, res) {
     res.sendFile(absolutePath);
