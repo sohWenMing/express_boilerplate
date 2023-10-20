da0a39390c76059ead3e14b0abbfcdfcb50248ef
Soh Wen Ming <wenming.soh@temus.com>
2023-10-20 10:55:36 +0800

    adding logs

--- myApp.js
+++ myApp.js
@@ -13,10 +13,12 @@ app.use('/public', express.static(staticPath));
 app.get('/json', function(req, res) {
     let message = "Hello json";
     if(process.env.MESSAGE_STYLE === "uppercase") {
-        console.log("uppercase");
+        console.log("enviroment variable is tracking correctly");
+        message = message.toUpperCase();
     }
     else{
         console.log(process.env.MESSAGE_STYLE);
+        console.log("environent variable is not tracking correctly");
     }
     res.json({"message": "Hello json"});
 })
